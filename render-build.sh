6dd255d8a3a42a8cee16d0a2212706d92041eb57
zappurenfuro <zarenfro23@gmail.com>
2024-06-23 01:00:05 +0700

    pleaseee

--- render-build.sh
+++ render-build.sh
@@ -6,5 +6,8 @@ cd /opt/render/project/.render/chrome
 curl -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt-get update && apt-get install -y ./google-chrome-stable_current_amd64.deb
 
+# Move back to project root
+cd /opt/render/project/src
+
 # Install Python dependencies
 pip install -r requirements.txt
