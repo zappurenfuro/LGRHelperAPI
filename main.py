bfb3758159e34f54e01c96a75ea75884339cd1df
zappurenfuro <zarenfro23@gmail.com>
2024-06-24 15:39:59 +0700

    v1.2 cookies and debugging

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
